861f9ca3a2347e0e1b8dffe4158522d84d53013e
Thomas Yates <tomy8s@hotmail.com>
2016-09-17 19:53:10 +0100

    nearly arrayized

--- directory.rb
+++ directory.rb
@@ -1,23 +1,23 @@
 student = [
- "Dr. Hanibal Lecter",
- "Darth Vader",
- "Nurse Ratched",
- "Michael Corleone",
- "Alex DeLarge",
- "The Wicked Witch of the West",
- "Terminator",
- "Freddy Krueger",
- "The Joker",
- "Joffrey Baratheon",
- "Normal Bates"
- ]
+    ["Dr. Hanibal Lecter", :november],
+    ["Darth Vader", :november],
+    ["Nurse Ratched", :november],
+    ["Michael Corleone", :november],
+    ["Alex DeLarge", :november],
+    ["The Wicked Witch of the West", :november],
+    ["Terminator", :november],
+    ["Freddy Krueger", :november],
+    ["The Joker", :november],
+    ["Joffrey Baratheon", :november],
+    ["Normal Bates", :november]
+]
 
 def print_header
     puts "The students of Villains Academy"
     puts "------------------"
 end
 def print(student)
-        student.each{ |s| puts s}
+        student.each{ |s| puts "#{s[0]} (#{s[1]} cohort)" }
 end
 def print_footer(list)
     puts "Overall, we have #{list.count} great students."
